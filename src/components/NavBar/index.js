ab1b5e70b0371a4102e76255b4158c9c631df2cd
Farzan Uddin <m.farzanuddin@gmail.com>
2023-07-29 18:35:05 +0400

    Removed unused vars

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,7 @@
-/* react */
-import { useState } from "react";
 /* nextjs */
 import Link from "next/link";
 import Image from "next/image";
 import { Source_Sans_3 as SansPro } from "next/font/google";
-/* lib */
-import clsx from "clsx";
 /* style */
 import styles from "./navbar.module.css";
 
@@ -26,42 +22,6 @@ const DesktopNavigation = () => {
 };
 
 const MobileNavigation = () => {
-  const [menuOpen, setMenuOpen] = useState();
-  return (
-    <>
-      <button
-        className={styles.mobileNavButton}
-        aria-label="open the navigation menu"
-        aria-controls="mobile-nav"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        <Image src="/icons/hamburger-menu.svg" alt="menu icon" width={30} height={30} />
-      </button>
-      <nav
-        id="mobile-nav"
-        className={`${Sans.className} ${styles.mobileNavMenu} ${clsx(menuOpen && styles.showMenu)}`}
-        aria-hidden={!menuOpen}
-      >
-        <ul className={styles.mobileNavList}>
-          <li className={styles.mobileNavItem}>
-            <div className={styles.mobileNavItemContainer}>
-              <a
-                href="/resume.pdf"
-                target="_blank"
-                rel="nonreferrer"
-                onClick={() => setMenuOpen(false)}
-              >
-                Resume
-              </a>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
-};
-
-const SingleNavigation = () => {
   return (
     <button
       className={styles.mobileNavButton}
@@ -83,8 +43,7 @@ export const NavBar = () => {
           <Image src="/Logo.svg" alt="farzan uddin logo" width={187} height={40} priority={true} />
         </Link>
         <DesktopNavigation />
-        {/* <MobileNavigation /> */}
-        <SingleNavigation />
+        <MobileNavigation />
       </div>
     </div>
   );
